6c8470c2d133bdf48858ff45ffd69ab9895339d6
JavadJadidi <javadjadidi@gmail.com>
2023-04-24 00:58:12 +0430

    implement-relation-ships

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class)->whereNull('parent_id');
+    }
 }
